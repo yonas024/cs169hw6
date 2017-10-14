677fb8a53b0fdac388678577f8adf555e2537a22
yonas024 <kbromyonas@berkeley.edu>
2017-10-14 06:32:52 +0000

    hw6-2

--- rottenpotatoes/db/schema.rb
+++ rottenpotatoes/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171013203638) do
+ActiveRecord::Schema.define(version: 20171013200944) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
